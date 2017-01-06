b863f9269fce09aa10c0256cfd1fb2fd6e2d0b33
Kothandapani Saravanan <pradeep-kumar.kothandapani-saravanan@metlife.com>
2017-01-06 10:46:49 -0500

    first commit

---
+++ product.js
@@ -0,0 +1,5 @@
+$( ".product-card .action a.btn-brand-2nd" ).click(function(e) {
+          e.preventDefault();
+          var title = $('.page-title__heading').text();
+          console.log( title );
+        });
